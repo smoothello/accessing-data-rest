66ce5987cd904b95f64aa4dd528dba8159b527b2
ntjp <nerk.tsu@gmail.com>
2024-11-03 19:27:31 +0900

    removed reliance on jdbcrepo because of sybase

--- src/main/java/com/example/accessingdatarest/Person.java
+++ src/main/java/com/example/accessingdatarest/Person.java
@@ -1,24 +1,14 @@
 // Person.java
 package com.example.accessingdatarest;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Column;
-import org.springframework.data.relational.core.mapping.Table;
 import lombok.Data;
 
-@Table("person")
 @Data
 public class Person {
-	@Id
-	@Column("id")
 	private Long id;
-	@Column("first_name")
 	private String firstName;
-	@Column("last_name")
 	private String lastName;
-	@Column("gender")
 	private String gender;
-	@Column("address")
 	private String address;
 
 	public Person(Long id, String firstName, String lastName, String gender, String address) {
